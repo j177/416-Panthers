6503a111bfe29249787dd4a1ab108aa6d0d5338d
j177 <55969215+j177@users.noreply.github.com>
2023-10-01 02:30:04 -0400

    fixed window undefined error

--- 416-project/src/app/Map.js
+++ 416-project/src/app/Map.js
@@ -1,37 +1,38 @@
-"use client"
 import { useEffect } from 'react';
 import L from 'leaflet';
-
 import 'leaflet/dist/leaflet.css';
-import statesGEO from './stateGeoJSON/states.geo.json'
+
+import statesGEO from './stateGeoJSON/states.geo.json';
 
 export default function Map() {
-    useEffect(() => {
-        const map = L.map('map').setView([37.8, -96], 4);
+  useEffect(() => {
+    if (typeof window !== 'undefined') {
+      const map = L.map('map').setView([37.8, -96], 4);
 
-        L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
-            attribution: '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors',
-        }).addTo(map);
+      L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
+        attribution: '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors',
+      }).addTo(map);
 
-        const geoJSONLayer = L.geoJSON(statesGEO, {
-            style: {
-                fillColor: 'green', 
-                fillOpacity: 0.5,   
-                color: 'black',    
-                weight: 1,      
-            },
-            onEachFeature: function (feature, layer) {
-                layer.on('click', function (e) {
-                    const stateName = feature.properties.name; 
-                    alert(`Clicked on ${stateName}`);
-                });
-            },
-        }).addTo(map);
+      const geoJSONLayer = L.geoJSON(statesGEO, {
+        style: {
+          fillColor: 'green',
+          fillOpacity: 0.5,
+          color: 'black',
+          weight: 1,
+        },
+        onEachFeature: function (feature, layer) {
+          layer.on('click', function (e) {
+            const stateName = feature.properties.name;
+            alert(`Clicked on ${stateName}`);
+          });
+        },
+      }).addTo(map);
 
-        return () => {
-            map.remove();
-        };
-    }, []);
+      return () => {
+        map.remove();
+      };
+    }
+  }, []);
 
-    return <div id="map" style={{ height: '600px', width: '100%' }}></div>;
+  return <div id="map" style={{ height: '600px', width: '100%' }}></div>;
 }
