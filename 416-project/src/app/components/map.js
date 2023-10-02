3c1c93f1e45725a3a10850f9ca89b004534a4861
j177 <55969215+j177@users.noreply.github.com>
2023-10-02 00:24:34 -0400

    added map to state pages, cannot refesh

--- 416-project/src/app/components/map.js
+++ 416-project/src/app/components/map.js
@@ -6,38 +6,53 @@ import { useRouter } from 'next/navigation';
 import L, { MapContainer, TileLayer, Marker, Popup } from 'leaflet';
 import 'leaflet/dist/leaflet.css';
 
+import { States } from '../constants/stateConstants'
 import statesGEO from '../stateGeoJSON/states.geo.json';
+import conGEO from '../stateGeoJSON/USA_118th_Congressional_Districts.geo.json'
 
-export default function Map( {state, zoom} ) {
-    const router = useRouter();
+export default function Map({ state, zoom }) {
+  const router = useRouter();
 
-    useEffect(() => {
-        const map = L.map('map').setView(state.coordinates, zoom);
+  useEffect(() => {
+    if (typeof window !== "undefined") {
+      const map = L.map('map').setView(state.coordinates, zoom);
 
-        L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
-            attribution: '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors',
-        }).addTo(map);
+      L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
+        attribution: '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors',
+      }).addTo(map);
 
-        const geoJSONLayer = L.geoJSON(statesGEO, {
-            style: {
-                fillColor: 'green', 
-                fillOpacity: 0.5,   
-                color: 'black',    
-                weight: 1,      
-            },
-            onEachFeature: function (feature, layer) {
-                layer.on('click', function (e) {
-                    const stateName = feature.properties.name;
+      const geoJSONLayer = L.geoJSON(statesGEO, {
+        style: {
+          fillColor: 'green',
+          fillOpacity: 0.5,
+          weight: 1,
+        },
+        onEachFeature: function (feature, layer) {
+          layer.on('click', function (e) {
+            const stateName = feature.properties.name;
 
-                    router.push('/' + stateName.replace(/ /g, ''));
-                });
-            },
-        }).addTo(map);
+            router.push('/' + stateName.replace(/ /g, ''));
+          });
+        },
+      }).addTo(map);
 
-        return () => {
-            map.remove();
-        };
-    }, [state, zoom]);
+      if (state != States.NOT_SELECTED) {
+        const conDis = L.geoJSON(conGEO, {
+          style: function (feature) {
+            const party = feature.properties.PARTY;
+            return {
+              color: party === 'Democrat' ? 'blue' : party === 'Republican' ? 'red' : 'gray',
+              weight: 2,
+            };
+          },
+        }).addTo(map)
+      }
 
-    return (<div id = "map"></div>);
+      return () => {
+        map.remove();
+      };
+    }
+  }, [state, zoom, router]);
+
+  return (<div id="map"></div>);
 }
--- 416-project/src/app/components/statePage.js
+++ 416-project/src/app/components/statePage.js
@@ -1,9 +1,12 @@
 import '../stylesheets/state.css'
 
-export default function StatePage() {
+import Map from './map.js';
+
+export default function StatePage({ state, zoom }) {
+
     return (
         <div className = "state-page-container">
-            <div className = "map-container">Map goes here</div>
+            <div className = "map-container"> <Map state = {state} zoom = {zoom}></Map></div>
             <div className = "data-container">Data goes here</div>
         </div>
     )
