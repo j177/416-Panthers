7fdced7894e7c2fab8d105ee688aee72795fb011
Aaron Liang <98434199+DancinCocobean@users.noreply.github.com>
2023-10-14 16:41:11 -0400

    Ensemble selection screen + ensemble v cluster graph)

---
+++ client/src/app/components/lineGraph.js
@@ -0,0 +1,26 @@
+import Plot from "react-plotly.js";
+
+export default function LineGraph({ data, title, xLabel, yLabel, width, height }) {
+    const layout = {
+        title: title,
+        titlefont: {
+            size: '20'
+        },
+        xaxis: {
+            title: xLabel
+        },
+        yaxis: {
+            title: yLabel
+        },
+        width: width,
+        height: height
+    };
+
+    return (
+        <Plot
+            data = {data}
+            layout = {layout}
+            config = {{ displayModeBar: false }}
+        />
+    );
+}
--- client/src/app/components/navbar.js
+++ client/src/app/components/navbar.js
@@ -3,7 +3,6 @@ import Navbar from 'react-bootstrap/Navbar';
 import NavDropdown from 'react-bootstrap/NavDropdown';
 import "bootstrap-icons/font/bootstrap-icons.css";
 
-
 import { States } from "../constants/stateConstants";
 import { useRouter } from "next/navigation";
 
@@ -12,14 +11,14 @@ export default function navigation() {
     return (
         <Navbar collapseOnSelect expand="sm" className="d-flex p-2 bg-body-tertiary">
             <Nav>
-                <li class = "nav-item me-3 me-sm-0">
-                    <a class="nav-link" onClick = {() => {router.push('/')}}>
-                        <i class="bi bi-house"></i>
+                <li className = "nav-item me-3 me-sm-0">
+                    <a className = "nav-link" onClick = {() => {router.push('/')}}>
+                        <i className ="bi bi-house"></i>
                     </a>
                 </li>
               <NavDropdown title="States" id="collapsible-nav-dropdown">
-                <NavDropdown.Item  onClick = {() => {router.push('/' + States.MICHIGAN.name)}}>Michigan</NavDropdown.Item>
-                <NavDropdown.Item  onClick = {() => {router.push('/' + States.NEW_YORK.name.replace(/ /g, ''))}}>
+                <NavDropdown.Item onClick = {() => {router.push('/' + States.MICHIGAN.name)}}>Michigan</NavDropdown.Item>
+                <NavDropdown.Item onClick = {() => {router.push('/' + States.NEW_YORK.name.replace(/ /g, ''))}}>
                   New York
                 </NavDropdown.Item>
                 <NavDropdown.Item onClick = {() => {router.push('/' + States.PENNSYLVANIA.name)}}>Pennsylvania</NavDropdown.Item>
